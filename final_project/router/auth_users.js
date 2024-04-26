2dc799672f67573176677f02e12f53591bac6b7c
Adithya Ajay <adithyaajay098@gmail.com>
2024-04-26 16:05:30 +0530

    task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -40,7 +40,7 @@ regd_users.post("/login", (req, res) => {
         res.json({ token });
     } else {
         // Return error message for invalid credentials
-        res.status(401).json({ message: 'Invalid username or password' });
+        res.status(401).json({ message: 'invalid ' });
     }
 });
 
@@ -77,6 +77,34 @@ regd_users.post("/review/:isbn", (req, res) => {
       res.status(404).json({ message: "Book not found" });
   }
 });
+
+
+// Route to delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn; // Get the ISBN from the request parameters
+  const username = req.session.username; // Get the username from the session
+
+  // Check if username is available in session
+  if (!username) {
+      return res.status(401).json({ message: 'User not logged in' });
+  }
+
+  // Check if the book exists
+  if (books[isbn]) {
+      // Check if the user has a review for the book
+      if (books[isbn].reviews[username]) {
+          // Delete the review
+          delete books[isbn].reviews[username];
+          res.status(200).json({ message: 'Review deleted successfully' });
+      } else {
+          res.status(404).json({ message: 'Review not found for this user' });
+      }
+  } else {
+      // If the book with the specified ISBN is not found, send a 404 Not Found response
+      res.status(404).json({ message: "Book not found" });
+  }
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
